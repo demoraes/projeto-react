4b39fd75e54c04641df95ef8fcede09583e73779
demoraes <gabrielmoraes_2013@outlook.com>
2020-11-07 00:44:26 -0400

    connection configuration with the API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import backgroundImage from './assets/gabriel.jpg';
 
 import Header from './components/Header';
+import api from './services/api';
 
 function App() {
-  const [projects, setProjects] = useState(['Primeiro projeto', 'Segundo projeto']);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
-    console.log(projects);
+  useEffect(() => {
+    async function loadProjects() {
+      const response = await api.get('repositories');
+
+      setProjects(response.data);
+    }
+    loadProjects();
+  }, []);
+
+
+  async function handleAddProject() {
+    const response = await api.post('repositories', {
+      title: 'nenem sendo nenem',
+      url: 'link do ennem'
+    });
+
+    const project = response.data;
+
+    //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    setProjects([...projects, project]);
   };
 
   return (
@@ -20,7 +38,7 @@ function App() {
       <img width={300} src={backgroundImage} />
 
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
